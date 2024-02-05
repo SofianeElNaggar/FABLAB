029b8e3f763c2c1583a51ba9dc812442d9359674
Mateo_Hany <mateo.hany@gmail.com>
2024-02-05 14:48:26 +0100

    fonctionnalités ouvrirfichier

--- Fonctionallités/file.mjs
+++ Fonctionallités/file.mjs
@@ -1,9 +1,8 @@
 import { exec } from 'child_process';
-import path from 'path';
 
 
 // Function to open a file with the default program on Windows
-const openFileOnWindows = (filePath) => {
+export const openFileOnWindows = (filePath) => {
   exec(`start "" "${filePath}"`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error opening the file: ${error.message}`);
@@ -12,7 +11,5 @@ const openFileOnWindows = (filePath) => {
 };
 
 
-const filePath = '/chemindufichier/'
-const absolutePath = path.resolve(filePath);
-
-openFileOnWindows(absolutePath);
+//const videoPath = "C:/Users/User/Videos/Captures/Mixer de volume - Haut-parleurs (Realtek(R) Audio) 2021-04-23 17-54-16.mp4";
+//openFileOnWindows(videoPath);
